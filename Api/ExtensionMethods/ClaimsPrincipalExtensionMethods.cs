616a3a1aea9efcc4c9e6ca4c18d65a82e97ea6cb
Abbas Ghebleh <abbas.ghebleh@gmail.com>
2023-11-09 16:50:19 +0330

    Two extension methods implemented for ClaimsPrincipal. RoleNames defined in statistics.

---
+++ Api/ExtensionMethods/ClaimsPrincipalExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using IpPanelSmsApi;
+using System.Security.Claims;
+
+namespace Api.ExtensionMethods;
+
+public static class ClaimsPrincipalExtensionMethods
+{
+    public static string? GetUserName(this ClaimsPrincipal value)
+    {
+        return value.FindFirstValue(ClaimTypes.Name);
+    }
+
+    public static string? GetUserId(this ClaimsPrincipal value)
+    {
+        return value.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
---
+++ Application/Common/Statics/RoleNames.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Common.Statics;
+
+public static class RoleNames
+{
+    public static string PowerUser = "PowerUser";
+    public static string Admin = "Admin";
+    public static string Mayor = "Mayor";
+    public static string Manager = "Manager";
+    public static string Executive = "Executive";
+    public static string Contractor = "Contractor";
+    public static string Operator = "Operator";
+    public static string Advertiser = "Advertiser";
+    public static string Inspector = "Inspector";
+    public static string Citizen = "Citizen";
+}
