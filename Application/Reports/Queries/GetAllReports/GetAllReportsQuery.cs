59eaa136cd8dce0933fda3e93702ba50b82523db
Abbas Ghebleh <abbas.ghebleh@gmail.com>
2023-11-11 19:48:34 +0330

    GetAllReports implemented in Application layer.

---
+++ Application/Reports/Queries/GetAllReports/GetAllReportsQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Interfaces.Persistence;
+using Domain.Models.Relational;
+using MediatR;
+
+namespace Application.Reports.Queries.GetReports;
+
+public sealed record GetAllReportsQuery(
+    PagingInfo PagingInfo,
+    int instanceId) : IRequest<PagedList<Report>>;
+
