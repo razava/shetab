e42999e592e14a78571338d748770cc92998b8a2
Abbas Ghebleh <abbas.ghebleh@gmail.com>
2023-11-30 19:09:34 +0330

    MapController implemented.

---
+++ Api/Controllers/MapController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+using Api.Abstractions;
+using Api.Dtos.ParsiMap;
+using Application.Maps.Queries.MapForwardQuery;
+using Mapster;
+using Application.Maps.Queries.MapBackwardQuery;
+using MediatR;
+
+namespace Shahrbin.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MapController : ApiController
+    {
+        protected MapController(ISender sender) : base(sender)
+        {
+        }
+
+        [HttpGet("Forward/{address}")]
+        public async Task<ActionResult<ForwardResult>> Forward(string address)
+        {
+            var query = new MapForwardQuery(address);
+            var result = await Sender.Send(query);
+            var r2 = result.Adapt<ForwardResult>();
+            return Ok(r2);
+        }
+
+        [HttpGet("Backward/{lng}/{lat}")]
+        public async Task<ActionResult<BackwardResult>> Backward(double lng, double lat)
+        {
+            var query = new MapBackwardQuery(lng, lat);
+            var result = await Sender.Send(query);
+            var r2 = result.Adapt<BackwardResult>();
+            return Ok(r2);
+        }
+    }
+}
