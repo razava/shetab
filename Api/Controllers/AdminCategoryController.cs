35d71392c5946cc75ef421456d02a127f3fc2ed4
zahra2112 <zahra.gholizadeh2112@gmail.com>
2023-12-12 11:13:32 +0330

    Admin Category Controller Fixed.

--- Api/Controllers/AdminCategoryController.cs
+++ Api/Controllers/AdminCategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Api.Abstractions;
 using Api.Contracts;
 using Application.Categories.Commands.AddCategory;
+using Application.Categories.Commands.UpdateCategory;
 using Application.Categories.Queries.GetCategory;
 using Application.Common.Interfaces.Persistence;
 using Domain.Models.Relational;
@@ -22,27 +23,27 @@ public class AdminCategoryController : ApiController
     //todo : .................Commands & queries ............
     //TODO: Define access policies
 
-    [Authorize]
-    [HttpGet]
-    public async Task<ActionResult<List<FlattenShortCategoryDto>>> GetCategories(int instanceId)
-    {
-        //todo : need a query that return flat list.............................................
-        //var query = new GetCategoryQuery(instanceId);
-        //var result = await Sender.Send(query);
-        //var mappedResult = result.Adapt<List<FlattenShortCategoryDto>>();
-        //return Ok(mappedResult);
-        await Task.CompletedTask;
-        return Ok();
-    }
+    //[Authorize]
+    //[HttpGet]
+    //public async Task<ActionResult<List<FlattenShortCategoryDto>>> GetCategories(int instanceId)
+    //{
+    //    //todo : need a query that return flat list.............................................
+    //    //var query = new GetCategoryQuery(instanceId);
+    //    //var result = await Sender.Send(query);
+    //    //var mappedResult = result.Adapt<List<FlattenShortCategoryDto>>();
+    //    //return Ok(mappedResult);
+    //    await Task.CompletedTask;
+    //    return Ok();
+    //}
 
-    [Authorize]
-    [HttpGet("All")]
-    public async Task<ActionResult<List<FlattenCategoryDto>>> GetAllCategories([FromQuery] QueryFilter queryFilter)
-    {
-        await Task.CompletedTask;
-        //Does GetCategoryQuery include categories list for this endpoint???
-        return Ok();
-    }
+    //[Authorize]
+    //[HttpGet("All")]
+    //public async Task<ActionResult<List<FlattenCategoryDto>>> GetAllCategories([FromQuery] QueryFilter queryFilter)
+    //{
+    //    await Task.CompletedTask;
+    //    //Does GetCategoryQuery include categories list for this endpoint???
+    //    return Ok();
+    //}
 
     /*
     [Authorize]
@@ -58,17 +59,7 @@ public class AdminCategoryController : ApiController
     [HttpPost]
     public async Task<ActionResult> CreateCategory(CategoryCreateDto categoryCreateDto)
     {
-        await Task.CompletedTask;
-        //AddCategoryCommand  need to complete......
-        return Ok();
-    }
-
-    [Authorize(Roles = "Admin")]
-    [HttpPut("{id:Guid}")]
-    public async Task<ActionResult> EditCategory(Guid id, CategoryCreateDto categoryCreateDto)
-    {
         var category = categoryCreateDto.Adapt<Category>();
-        //todo : AddCategoryCommand  need to complete......
         var command = new AddCategoryCommand(category);
         var result = await Sender.Send(command);
         if (result == null)
@@ -76,14 +67,27 @@ public class AdminCategoryController : ApiController
         return Ok();
     }
 
-
     [Authorize(Roles = "Admin")]
-    [HttpDelete("{id:Guid}")]
-    public async Task<ActionResult> DeleteCategory(Guid id)
+    [HttpPut("{id:Guid}")]
+    public async Task<ActionResult> EditCategory(Guid id, CategoryCreateDto categoryCreateDto)
     {
-        await Task.CompletedTask;
+        //todo : review
+        var category = categoryCreateDto.Adapt<Category>();
+        var command = new UpdateCategoryCommand(category);
+        var result = await Sender.Send(command);
+        if (result == null)
+            return Problem();
         return Ok();
     }
+
+
+    //[Authorize(Roles = "Admin")]
+    //[HttpDelete("{id:Guid}")]
+    //public async Task<ActionResult> DeleteCategory(Guid id)
+    //{
+    //    await Task.CompletedTask;
+    //    return Ok();
+    //}
 
 
 }
