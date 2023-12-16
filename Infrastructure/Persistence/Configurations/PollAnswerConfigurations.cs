e9456c9abf464f8802c03f4413b451ecb6f65617
zahra2112 <zahra.gholizadeh2112@gmail.com>
2023-12-16 14:38:39 +0330

    PollAnswer Configuration modified.

--- Infrastructure/Persistence/Configurations/PollAnswerConfigurations.cs
+++ Infrastructure/Persistence/Configurations/PollAnswerConfigurations.cs
@@ -8,6 +8,6 @@ public class PollAnswerConfigurations : IEntityTypeConfiguration<PollAnswer>
 {
     public void Configure(EntityTypeBuilder<PollAnswer> builder)
     {
-
+        builder.HasOne(p => p.Poll).WithMany(e => e.Answers).OnDelete(DeleteBehavior.NoAction);
     }
 }
---
+++ Infrastructure/Persistence/Migrations/20231216104212_PollAnswerConfigurationModified.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PollAnswerConfigurationModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PollAnswer_Poll_PollId",
+                table: "PollAnswer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PollAnswer_Poll_PollId",
+                table: "PollAnswer",
+                column: "PollId",
+                principalTable: "Poll",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PollAnswer_Poll_PollId",
+                table: "PollAnswer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PollAnswer_Poll_PollId",
+                table: "PollAnswer",
+                column: "PollId",
+                principalTable: "Poll",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
