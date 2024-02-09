f2ea4e75fba11d87d574aeabb5f1107798d04636
Abbas Ghebleh <abbas.ghebleh@gmail.com>
2024-02-09 19:04:19 +0330

    Database updated.

---
+++ Infrastructure/Persistence/Migrations/20240209153402_InstanceIdAddedToForm.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstanceIdAddedToForm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ShahrbinInstanceId",
+                table: "Form",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Form_ShahrbinInstanceId",
+                table: "Form",
+                column: "ShahrbinInstanceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Form_ShahrbinInstance_ShahrbinInstanceId",
+                table: "Form",
+                column: "ShahrbinInstanceId",
+                principalTable: "ShahrbinInstance",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Form_ShahrbinInstance_ShahrbinInstanceId",
+                table: "Form");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Form_ShahrbinInstanceId",
+                table: "Form");
+
+            migrationBuilder.DropColumn(
+                name: "ShahrbinInstanceId",
+                table: "Form");
+        }
+    }
+}
