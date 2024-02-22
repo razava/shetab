8c3a786dba7447a6669c07647cde2be8cf4cda98
Abbas Ghebleh <abbas.ghebleh@gmail.com>
2024-02-23 02:05:57 +0330

    Migrations for category's default priority.

---
+++ Infrastructure/Persistence/Migrations/20240222223513_DefaultPriorityAddedToCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefaultPriorityAddedToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultPriority",
+                table: "Category",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultPriority",
+                table: "Category");
+        }
+    }
+}
