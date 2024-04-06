a85a6cf58b63ce9766a255ff7aab627826700e7e
zahra <zahra.gholizadeh2112@gmail.com>
2024-04-06 14:49:11 +0330

    SpecifyReplyComments Added and some revisions(?).

---
+++ Application/Setup/Commands/SpecifyReplyComments/SpecifiyReplyCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces.Persistence;
+using Domain.Models.Relational.ReportAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Setup.Commands.SpecifyReplyComments;
+
+internal class SpecifiyReplyCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<SpecifiyReplyCommand, Result<bool>>
+{
+    public async Task<Result<bool>> Handle(SpecifiyReplyCommand request, CancellationToken cancellationToken)
+    {
+        var context = unitOfWork.DbContext;
+        var replyCommentIds = await context.Set<Comment>()
+            .AsNoTracking()
+            .Where(c => c.ShahrbinInstanceId == request.instanceId && c.ReplyId != null)
+            .Select(c => c.ReplyId)
+            .ToListAsync();
+
+        var replyComments = await context.Set<Comment>()
+            .Where(c => replyCommentIds.Contains(c.Id))
+            .ToListAsync();
+
+        foreach (var comment in replyComments)
+        {
+            comment.IsReply = true;
+        }
+
+        context.Set<Comment>().AttachRange(replyComments);
+        await unitOfWork.SaveAsync();
+
+        return true;
+    }
+}
