641f484ded586de530dc555f1024ea8e3874eb88
RKH <raziehkhanzadeh77@gmail.com>
2024-09-16 11:04:22 +0330

    Updating satisfaction report when registering satisfaction

--- Application/Satisfactions/Commands/UpsertSatisfaction/UpsertSatisfactionCommandHandler.cs
+++ Application/Satisfactions/Commands/UpsertSatisfaction/UpsertSatisfactionCommandHandler.cs
@@ -1,17 +1,23 @@
 ﻿using Application.Common.Helper;
 using Application.Common.Interfaces.Persistence;
 using Domain.Models.Relational;
+using Domain.Models.Relational.Common;
 using SharedKernel.Successes;
 
 namespace Application.Satisfactions.Commands.UpsertSatisfaction;
 
 internal sealed class UpsertSatisfactionCommandHandler(
     ISatisfactionRepository satisfactionRepository,
-    IUnitOfWork unitOfWork)
+    IUnitOfWork unitOfWork,
+    IReportRepository reportRepository)
     : IRequestHandler<UpsertSatisfactionCommand, Result<SatisfactionResponse>>
 {
     public async Task<Result<SatisfactionResponse>> Handle(UpsertSatisfactionCommand request, CancellationToken cancellationToken)
     {
+        var report = await reportRepository.GetByIDAsync(request.ReportId);
+        if (report == null)
+            return NotFoundErrors.Report;
+
         Satisfaction? satisfaction;
         satisfaction = await satisfactionRepository.GetSingleAsync(s => s.ReportId == request.ReportId);
         if (satisfaction is null)
@@ -29,6 +35,8 @@ internal sealed class UpsertSatisfactionCommandHandler(
             satisfaction.Update(request.UserId, request.Comment, request.Rating);
             satisfactionRepository.Update(satisfaction);
         }
+
+        report.UpdateFeedback(request.Rating);
 
         await unitOfWork.SaveAsync();
 
